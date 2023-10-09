4b234055bc9f01344dc6e671e551a84d5d1979c0
Alizgaa <alizgaa@yandex.ru>
2023-10-09 10:32:53 +0800

    task_03

--- src/JavaSyntax/Array/task_03.java
+++ src/JavaSyntax/Array/task_03.java
@@ -1,4 +1,14 @@
 package JavaSyntax.Array;
 
 public class task_03 {
+    public static int[] array = new int[]{-1, 2, 3, -4, -4, 3};
+
+    public static void main(String[] args) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (-1) * array[i];
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
 }
