7aaf5c6b8387a0d736429287204a9c09f7a2fd3a
Alizgaa <alizgaa@yandex.ru>
2023-10-09 09:38:54 +0800

    task_02 solved

--- src/JavaSyntax/Array/task_02.java
+++ src/JavaSyntax/Array/task_02.java
@@ -1,4 +1,18 @@
 package JavaSyntax.Array;
 
 public class task_02 {
+    static String[] strings = new String[5];
+
+    public static void main(String[] args) {
+        for (int i = 0; i < strings.length; i++) {
+            if (i % 2 == 0) {
+                strings[i] = "Чётный";
+            } else {
+                strings[i] = "Нечётный";
+            }
+        }
+        for (int i = 0; i < strings.length; i++) {
+            System.out.println(strings[i]);
+        }
+    }
 }
